35421a0fe8232395409f1bde81ef5c2b83b2fa5f
inesJ <ines_j@gmx.net>
2013-04-29 23:10:16 +0300

    Update bannerdata.js

--- js/bannerdata.js
+++ js/bannerdata.js
@@ -1,13 +1,16 @@
 function loadBannerData(){
-  var docUrl = 'https://spreadsheets.google.com/feeds/cells/0AjpZIpAGKxCEdGlfQ2JkNVlnT2NTcEZiOFFDOUNNcGc/od6/public/basic?alt=json';
+	var docUrl = 'https://spreadsheets.google.com/feeds/list/0AjpZIpAGKxCEdGlfQ2JkNVlnT2NTcEZiOFFDOUNNcGc/od6/public/values?alt=json'
 	$.getJSON(docUrl, function(data) {
 		var entries = data.feed.entry;
 		var html = '';
-		for(var i = 1; i<=entries.length; i+=2){
-			var output = entries[i].content.$t;
-			html = html+('<p>'+output+'</p>');
-			//
-		}
+		$.each(entries, function(i){
+			var bid = entries[i].gsx$bannerid.$t;
+			var loc = entries[i].gsx$locale.$t;
+			html = '<p>'+bid+'</p><p>'+loc+'</p>';
+			$('.ad').attr('id',bid).attr('data-loc',loc);
+			
+		});
 		$('.text').wrapInner(html);
 	});
+
 }
